89b589174d66b4aba653132d69347a57177335a7
Jonathan Fisher <jpfisher72@gmail.com>
2021-01-08 14:02:37 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
    public static void main(String[] args)
    {
       System.out.println("Hello World! I've been at Cal Poly for 2.5 years.");
+      System.out.println("Jonathan's also here to say hi!");
    }
 }
